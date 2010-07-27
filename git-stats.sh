b1ee2cf5577082afc9cbe1e0bd264180da8f73fc
Valentin Haenel <valentin.haenel@gmx.de>
2010-07-27 23:12:47 +0200

    start and end date in one place only

--- git-stats.sh
+++ git-stats.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+START=18.07.2010
+END=25.07.2010
 echo "Number of commits per author:"
-git --no-pager shortlog --after=18.07.2010 --before=24.07.2010  -sn --all
-AUTHORS=$( git shortlog --after=18.07.2010 --before=24.07.2010  -sn --all | cut -f2 | cut -f1 -d' ' ; echo 'Unknowen')
+git --no-pager shortlog --after=$START --before=$END  -sn --all
+AUTHORS=$( git shortlog --after=$START --before=$END  -sn --all | cut -f2 | cut -f1 -d' ' ; echo 'Unknowen')
 for a in $AUTHORS:
 do
     echo '-------------------'
     echo "Statistics for: $a"
     echo -n "Number of files changed: "
-    git log --after=18.07.2010 --before=25.07.2010 --all --numstat --format="%n" --author=$a | cut -f3 | sort -u | wc -l
+    git log --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
     echo -n "Number of lines added: "
-    git log --after=18.07.2010 --before=25.07.2010 --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
+    git log --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
     echo -n "Number of lines deleted: "
-    git log --after=18.07.2010 --before=25.07.2010 --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
+    git log --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
 done
