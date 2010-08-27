a47b3347c6cf71262038698e2549429307ddffcd
Zbyszek Szmek <zbyszek@in.waw.pl>
2010-08-27 19:57:46 +0200

    add some basic options

--- git-stats.sh
+++ git-stats.sh
@@ -4,14 +4,27 @@ END=25.07.2010
 echo "Number of commits per author:"
 git --no-pager shortlog --after=$START --before=$END  -sn --all
 AUTHORS=$( git shortlog --after=$START --before=$END  -sn --all | cut -f2 | cut -f1 -d' ' ; echo 'Unknowen')
+LOGOPTS=""
+if [ "$1" == '-w' ]; then
+    LOGOPTS="$LOGOPTS -w"
+    shift
+fi
+if [ "$1" == '-M' ]; then
+    LOGOPTS="$LOGOPTS -M"
+    shift
+fi
+if [ "$1" == '-C' ]; then
+    LOGOPTS="$LOGOPTS -C --find-copies-harder"
+    shift
+fi
 for a in $AUTHORS:
 do
     echo '-------------------'
     echo "Statistics for: $a"
     echo -n "Number of files changed: "
-    git log --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
+    git log $LOGOPTS --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
     echo -n "Number of lines added: "
-    git log --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
+    git log $LOGOPTS --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
     echo -n "Number of lines deleted: "
-    git log --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
+    git log $LOGOPTS --after=$START --before=$END --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
 done
